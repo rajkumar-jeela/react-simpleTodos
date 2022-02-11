e956c9328ce3a76a34c50ce4ac5beff7ef984e0f
rajkumar-jeela <rajkumarjeela786@gmail.com>
2022-02-11 16:25:08 +0000

    first commit

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,78 +1,126 @@
 import {Component} from 'react'
-
+import {MdDeleteForever} from 'react-icons/md'
+import {IoIosSave} from 'react-icons/io'
 import TodoItem from '../TodoItem'
 
 import './index.css'
 
-const initialTodosList = [
-  {
-    id: 1,
-    title: 'Book the ticket for today evening',
-  },
-  {
-    id: 2,
-    title: 'Rent the movie for tomorrow movie night',
-  },
-  {
-    id: 3,
-    title: 'Confirm the slot for the yoga session tomorrow morning',
-  },
-  {
-    id: 4,
-    title: 'Drop the parcel at Bloomingdale',
-  },
-  {
-    id: 5,
-    title: 'Order fruits on Big Basket',
-  },
-  {
-    id: 6,
-    title: 'Fix the production issue',
-  },
-  {
-    id: 7,
-    title: 'Confirm my slot for Saturday Night',
-  },
-  {
-    id: 8,
-    title: 'Get essentials for Sunday car wash',
-  },
-]
-
-// Write your code here
-class SimpleTodos extends Component {
+class Todo extends Component {
   state = {
-    todosList: initialTodosList,
+    todoList: [],
+    inputElement: '',
   }
 
-  deleteTodo = id => {
-    const {todosList} = this.state
-    const updatedTodoList = todosList.filter(eachTodo => eachTodo.id !== id)
+  componentDidMount() {
+    this.getTodoListFromLocalStorage()
+  }
 
-    this.setState({
-      todosList: updatedTodoList,
-    })
+  getTodoListFromLocalStorage = () => {
+    const stringifiedTodoList = localStorage.getItem('todoList')
+    const parsedTodoList = JSON.parse(stringifiedTodoList)
+    if (parsedTodoList === null) {
+      this.setState({todoList: []})
+    } else {
+      this.setState({todoList: parsedTodoList})
+    }
+  }
+
+  onClickSubmit = event => {
+    event.preventDefault()
+    console.log('submit button is clicked ')
+
+    const {inputElement} = this.state
+    if (inputElement === '') {
+      alert.show('Please Enter your todo')
+    } else {
+      const todoContent = inputElement
+      this.setState(prevState => ({
+        todoList: [...prevState.todoList, todoContent],
+      }))
+      this.setState({inputElement: ''})
+    }
+  }
+
+  onChangeInput = event => {
+    this.setState({inputElement: event.target.value})
+  }
+
+  onClickDeleteAllTodo = () => {
+    this.setState({todoList: []})
+    localStorage.removeItem('todoList')
+  }
+
+  onClickDeleteTodo = content => {
+    const {todoList} = this.state
+    const newList = todoList.filter(eachItem => eachItem !== content)
+    this.setState({todoList: newList})
+  }
+
+  onClickSaveAllTodo = () => {
+    const {todoList} = this.state
+    localStorage.setItem('todoList', JSON.stringify(todoList))
   }
 
   render() {
-    const {todosList} = this.state
+    const {inputElement, todoList} = this.state
+    const showTodoContainer = todoList.length > 0
+    console.log(showTodoContainer)
     return (
-      <div className="app-container">
-        <div className="simple-todos-container">
-          <h1 className="heading">Simple Todos</h1>
-          <ul className="todos-list">
-            {todosList.map(eachTodo => (
-              <TodoItem
-                key={eachTodo.id}
-                todoDetails={eachTodo}
-                deleteTodo={this.deleteTodo}
-              />
-            ))}
-          </ul>
+      <div className="main-todo-container">
+        <h1 className="todo-main-heading">Todos</h1>
+        <div className="todo-container">
+          <h1 className="create-task-heading">Create Task</h1>
+          <form onSubmit={this.onClickSubmit}>
+            <input
+              type="text"
+              className="todo-input-tag"
+              placeholder="What needs to be done?"
+              onChange={this.onChangeInput}
+              value={inputElement}
+            />
+            <button type="submit" className="add-todo-button">
+              Add Todo
+            </button>
+          </form>
         </div>
+        {showTodoContainer && (
+          <div className="todo-lists-container">
+            <div className="todo-list-heading-container">
+              <h1 className="create-task-heading">Your Todos</h1>
+              <div className="save-delete-button-container">
+                <button
+                  type="button"
+                  className="save-all-todo-button"
+                  onClick={this.onClickSaveAllTodo}
+                >
+                  <IoIosSave color="green" size={20} />
+                  <span className="button-text">Save All Todos</span>
+                </button>
+
+                <button
+                  className="delete-all-todo-button"
+                  type="button"
+                  onClick={this.onClickDeleteAllTodo}
+                >
+                  <MdDeleteForever color="red" size={20} />
+                  <span className="button-text">Delete All todo</span>
+                </button>
+              </div>
+            </div>
+            <ul>
+              {todoList.map(eachItem => (
+                <TodoItem
+                  todoItemContent={eachItem}
+                  key={eachItem}
+                  onClickDeleteTodo={this.onClickDeleteTodo}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     )
   }
 }
 
-export default SimpleTodos
+export default Todo
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,18 @@
-// Write your code here
+import {MdDeleteForever} from 'react-icons/md'
 import './index.css'
 
 const TodoItem = props => {
-  const {todoDetails, deleteTodo} = props
-  const {id, title} = todoDetails
+  const {todoItemContent, onClickDeleteTodo} = props
 
-  const onDeleteTodo = () => {
-    deleteTodo(id)
+  const onClickDelete = () => {
+    onClickDeleteTodo(todoItemContent)
   }
 
   return (
-    <li className="todo-item">
-      <p className="title">{title}</p>
-      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
-        Delete
+    <li className="todo-list-item">
+      <p className="todo-para">{todoItemContent}</p>
+      <button type="button" className="todo-delete" onClick={onClickDelete}>
+        <MdDeleteForever size={20} color="#4ce0d2" />
       </button>
     </li>
   )
